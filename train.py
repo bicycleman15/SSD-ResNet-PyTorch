70c03cf56936c8ef0fab7426b1752a96d0b88179
neelabh17 <neelabh.madan@outlook.com>
2020-07-23 01:10:51 +0530

    arg parser added

--- train.py
+++ train.py
@@ -1,13 +1,22 @@
 from lightning.lightning_model import SSD300_COCO
 from omegaconf import OmegaConf
 import pytorch_lightning as pl
-
 from lightning.utils import set_seed
 import argparse
 
-# TODO :- remove config and replace with cfg yaml file (done)
-# TODO :- make train working with scheduler and everything, logs too (done)
-# TODO :- see saving model to take up less space (done)
+# TODO : add gpus
+parser = argparse.ArgumentParser(description='Lightning SSD Training')
+parser.add_argument('--gpus', default='0', 
+    help='provide gpus in comma separeated fashion eg for gpus 1,2 provide --gpus 1,2')
+args = parser.parse_args()
+
+# asserting corect input for gpus
+gpu_list=args.gpus.split(",")
+assert len(gpu_list)== sum(list(map(lambda x: x.isnumeric(),gpu_list)))
+
+# creating a numeric gpu list
+gpu_list=list(map(int ,gpu_list))
+
 # TODO :- make inference.py file/functions so that can work with it in validation_end() func, and calc AP (remaining)
 
 if __name__ == '__main__':
@@ -21,10 +30,8 @@ if __name__ == '__main__':
     print('Loading Model....')
     model = SSD300_COCO(cfg=config)
 
-    # TODO : add gpus
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('gpus', default=1)
-
+    
+    
     # Create model save checkpoint
     save_checkpoint = pl.callbacks.ModelCheckpoint(
         monitor='val_loss',
@@ -33,13 +40,28 @@ if __name__ == '__main__':
     )
     lr_logger = pl.callbacks.LearningRateLogger()
 
-    trainer = pl.Trainer(
-        checkpoint_callback=save_checkpoint,
-        callbacks=[lr_logger],
-        gpus=1,
-        train_percent_check=0.01,
-        val_percent_check=0.01
-    )
+    # Finding gpu configuration
+
+    if(len(gpu_list)>1):
+        # Use multi gpu training
+
+        trainer = pl.Trainer(
+            checkpoint_callback=save_checkpoint,
+            callbacks=[lr_logger],
+            gpus=gpu_list,
+            train_percent_check=0.01,
+            val_percent_check=0.01,
+            distributed_backend='ddp'
+        )
+    else:
+        trainer = pl.Trainer(
+            checkpoint_callback=save_checkpoint,
+            callbacks=[lr_logger],
+            gpus=gpu_list,
+            train_percent_check=0.01,
+            val_percent_check=0.01,
+        )
+
 
     trainer.fit(model)
 
